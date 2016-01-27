c2b264156317daf438e97bfa8e7162f64ca29cee
Kristaps Grinbergs <kristaps@qminderapp.com>
2016-01-27 22:05:31 +0200

    Initial Commit

---
+++ TartuWeather/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TartuWeather
+//
+//  Created by Kristaps Grinbergs on 27/01/16.
+//  Copyright © 2016 fassko. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
